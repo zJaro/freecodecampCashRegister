8d5024db820191eea56a11ce86a76be7c76a51ba
zJaro <61804452+zJaro@users.noreply.github.com>
2025-02-03 14:03:19 +0100

    Make small changes to functions, move as many variables inside of funtions as possible

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-let cid = [
-  ['PENNY', 0],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
-];
+let cid = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]];
 
 const moneyValue = [
   ['PENNY', 0.01],
@@ -32,18 +22,18 @@ const priceInput = document.getElementById("price");
 
 let cash = 0;
 let price = 1.87;
-let change = cash - price;
-let diff = price - cash;
-let registerMessage = ``;
+
+
+
 
 cashBtn.addEventListener("click",() =>{
-    cash = cashInput.value;
+    cash = Number(cashInput.value);
     console.log(`Cash from customer ${cash}`)
     checkPrice();
 })
 
 priceBtn.addEventListener("click",() =>{
-    price = priceInput.value;
+    price = Number(priceInput.value);
     updateTotal();
     console.log(`Price in store ${price}`)
 })
@@ -53,30 +43,68 @@ const updateTotal = () => {
 };
 
 const checkPrice = () => {
+  let registerMessage = ``;
+    let totalCashInRegister = cid.reduce((sum, e) => sum + e[1], 0);
+    console.log(totalCashInRegister)
     console.log(`Cash: ${cash}`);
     console.log(`Price: ${price}`);
     if (price > cash){
         alert("Customer does not have enough money to purchase the item");
     }
-    else if (price == cash){
-        alert("No change due - customer paid with exact cash");
-        changeDue.value ="No change due - customer paid with exact cash";
+    else if (price === cash){
         changeDue.innerHTML ="No change due - customer paid with exact cash";
+        alert("No change due - customer paid with exact cash");
+        
     } 
     else{
-      change = cash - price;
-      for (let i = cid.length - 1; i >= 0; i--) {
-        console.log(`Change to give back $${change}`);
-        change = changeCount(change, moneyValue[i][1], i);
-        console.log(change);
+      const moneyValue = [
+        ['PENNY', 0.01],
+        ['NICKEL', 0.05],
+        ['DIME', 0.1],
+        ['QUARTER', 0.25],
+        ['ONE', 1],
+        ['FIVE', 5],
+        ['TEN', 10],
+        ['TWENTY', 20],
+        ['ONE HUNDRED', 100]
+      ];
+      
+      let change = cash - price;
+      let change2 = change;
+      let temp
+      if(totalCashInRegister < change){
+        changeDue.innerHTML=`Status: INSUFFICIENT_FUNDS`;
+        return 0;
       }
-      changeDue.innerHTML=`Status: OPEN ${registerMessage}`;
+      else{
+        for (let i = cid.length - 1; i >= 0; i--) {
+          console.log(`Change to give back $${change}`);
+          temp = changeCount(change, moneyValue[i][1], i);
+          change = temp[0];
+          registerMessage += temp[1];
+          console.log(change);
+        }
+        if(change === 0 && totalCashInRegister === change2 ){
+          changeDue.innerHTML=`Status: CLOSED ${registerMessage}`;
+        }
+        else if(change === 0 ){
+          changeDue.innerHTML=`Status: OPEN ${registerMessage}`;
+        }
+        else{
+        changeDue.innerHTML=`Status: INSUFFICIENT_FUNDS`;
+        }
+    
+
+      }
     }
     
   };
 
 
+
+
 const changeCount = (change, cashType, cidPlace) =>{
+  let registerMessage = '';
   const valueOfCashTypeInRegister = cid[cidPlace][1];
   const amountOfCashTypeInRegister = valueOfCashTypeInRegister / cashType;
   const amountOfCashTypeInChange = Math.floor(change / cashType);
@@ -98,10 +126,10 @@ const changeCount = (change, cashType, cidPlace) =>{
     registerMessage += `${cid[cidPlace][0]}: $${cashType * amountOfCashTypeInRegister} `;}
 
   }
-  change = change.toFixed(2)
+  change = Number(change.toFixed(2));
   console.log(`Change still: ${change}`)
   
-  return change;
+  return [change, registerMessage];
 }
 
 
